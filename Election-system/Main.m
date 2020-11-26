023b5f4fef6617950bba82b739b1dc6edacb17fb
Eken-KN <60141649+Eken-KN@users.noreply.github.com>
2020-11-26 15:18:58 +0100

    Update Main.m

--- Election-system/Main.m
+++ Election-system/Main.m
@@ -2,3 +2,39 @@
 
 %Test message
 % New stuff :D
+
+nParameter = 10;
+nIndividual = 100;
+nParty = 10;
+
+nIteration = 1000;
+
+parameters = InitializeParameters(nParameter);
+
+population = InitializePopulation(nIndividual, nParameter, nParty); %population is matrix with size nPopulation x (2 + nParameter + nParty)
+
+parties = InitializeParties(nParty, nParameter);
+
+
+
+populationPlot = InitializePopulationPlot(population);
+
+countryPlot = InitializeCountryPlot(parameters, parties);
+
+for i = 1:nIteration
+    
+    opinions = population(:, (3 + nParameter):(2 + nParameter + nParty));
+    
+    [newParameters, government] = RunElection(parties, opinions, parameters);
+    
+    UpdateCountryPlot(newParameters, government);
+    
+    newOpinions = ChangeOpinion(opinions, government, newParameters, parameters);
+    
+    population(:, (3 + nParameter):(2 + nParameter + nParty)) = newOpinions;
+    
+    population = CreateNextGeneration(population);
+    
+    UpdatePopulationPlot(populationPlot, population);
+    
+end
