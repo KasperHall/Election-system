f2967f751dcff1b49e106dbda910f040e9c56bf5
Eken-KN <60141649+Eken-KN@users.noreply.github.com>
2020-12-04 13:48:09 +0100

    Made even faster

--- Election-system/CreateNextGeneration.m
+++ Election-system/CreateNextGeneration.m
@@ -1,16 +1,34 @@
 function population = CreateNextGeneration(population, percentageToUpdate, neighbourhoodSize, gridSize, nParameter, nParty)
 
     nOfNewIndividuals = floor(size(population,1)*percentageToUpdate);
-    distanceMatrix = GetDistance(population);
-    newIndividuals = zeros(nOfNewIndividuals, size(population,2));
+%     distanceMatrix = GetDistance(population);
+%     newIndividuals = zeros(nOfNewIndividuals, size(population,2));
+%     
+%     possiblePairs = sparse((distanceMatrix < neighbourhoodSize) & ~logical(eye(size(population,1))));
+%     
+%     [is, js] = find(possiblePairs);
+%     iTemp = randi([1, size(is,1)], nOfNewIndividuals, 1);
+%     
+%     iParent1 = is(iTemp);
+%     iParent2 = js(iTemp);
     
-    possiblePairs = sparse((distanceMatrix < neighbourhoodSize) & ~logical(eye(size(population,1))));
+    iParent1 = zeros(nOfNewIndividuals, 1);
+    iParent2 = zeros(nOfNewIndividuals, 1);
+    nLeft = nOfNewIndividuals;
     
-    [is, js] = find(possiblePairs);
-    iTemp = randi([1, size(is,1)], nOfNewIndividuals, 1);
-    
-    iParent1 = is(iTemp);
-    iParent2 = js(iTemp);
+    while nLeft > 0
+        iProposedParent1 = randi([1, size(population,1)], nLeft, 1);
+        iProposedParent2 = randi([1, size(population,1)], nLeft, 1);
+        
+        %Check that proposed parents are close enough
+        iAccepted = ((population(iProposedParent1, 1) - population(iProposedParent2, 1)).^2 + (population(iProposedParent1, 2) - population(iProposedParent2, 2)).^2 < neighbourhoodSize^2) & (iProposedParent1 ~= iProposedParent2);
+        nAccepted = sum(iAccepted, 1);
+        
+        iParent1((nLeft - nAccepted + 1):nLeft) = iProposedParent1(iAccepted);
+        iParent2((nLeft - nAccepted + 1):nLeft) = iProposedParent2(iAccepted);
+        
+        nLeft = nLeft - nAccepted;
+    end
     
 %     cumPossible = cumsum(possiblePairs(:)');
 %     cumProb = cumPossible / max(cumPossible);
