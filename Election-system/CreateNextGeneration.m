c4941ef3006050ca5be305c412ff69f6c6c9b323
Eken-KN <60141649+Eken-KN@users.noreply.github.com>
2020-12-07 11:43:32 +0100

    Fixed death of parents

--- Election-system/CreateNextGeneration.m
+++ Election-system/CreateNextGeneration.m
@@ -1,16 +1,6 @@
 function population = CreateNextGeneration(population, percentageToUpdate, neighbourhoodSize, gridSize, nParameter, nParty, parameterDeviation, opinionDeviation, positionDeviation)
 
     nOfNewIndividuals = floor(size(population,1)*percentageToUpdate);
-%     distanceMatrix = GetDistance(population);
-%     newIndividuals = zeros(nOfNewIndividuals, size(population,2));
-%     
-%     possiblePairs = sparse((distanceMatrix < neighbourhoodSize) & ~logical(eye(size(population,1))));
-%     
-%     [is, js] = find(possiblePairs);
-%     iTemp = randi([1, size(is,1)], nOfNewIndividuals, 1);
-%     
-%     iParent1 = is(iTemp);
-%     iParent2 = js(iTemp);
     
     iParent1 = zeros(nOfNewIndividuals, 1);
     iParent2 = zeros(nOfNewIndividuals, 1);
@@ -28,28 +18,22 @@ function population = CreateNextGeneration(population, percentageToUpdate, neigh
         iParent1((nLeft - nAccepted + 1):nLeft) = iProposedParent1(iAccepted);
         iParent2((nLeft - nAccepted + 1):nLeft) = iProposedParent2(iAccepted);
         
-        nLeft = nLeft - nAccepted;
-        
         if counter >= 100
             
             iParent1(1:nLeft) = iProposedParent1;
-            iParent2(1:nLeft) = iProposedParent2;
+            iParent2(1:nLeft) = iProposedParent1;
             break;
         end
+        
+        nLeft = nLeft - nAccepted;
+        
         counter = counter + 1;
     end
-    
-%     cumPossible = cumsum(possiblePairs(:)');
-%     cumProb = cumPossible / max(cumPossible);
-%     
-%     iBothParents = sum(rand(nOfNewIndividuals, 1) < cumProb, 2) + 1;
-%     
-%     iParent1 = mod(iBothParents - 1, size(population,1)) + 1;
-%     iParent2 = floor((iBothParents - 1)/size(population,1)) + 1;
 
     newIndividuals = GenerateIndividual(population(iParent1,:), population(iParent2,:), neighbourhoodSize, gridSize, nParameter, nParty, parameterDeviation, opinionDeviation, positionDeviation);
     
-    individualsToRemove = randperm(size(population,1),nOfNewIndividuals);
+%     individualsToRemove = randperm(size(population,1),nOfNewIndividuals);
+    individualsToRemove = iParent1;
     population(individualsToRemove,:) = newIndividuals;
         
 end
