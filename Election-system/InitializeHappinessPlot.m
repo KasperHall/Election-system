d13f47c4ec04c597f8b1d2f09f55fd649acc326b
FoadAlhayek <FoadAlhayek@users.noreply.github.com>
2020-12-09 17:09:16 +0100

    Update InitializeHappinessPlot.m

--- Election-system/InitializeHappinessPlot.m
+++ Election-system/InitializeHappinessPlot.m
@@ -3,7 +3,7 @@ function happinessPlot = InitializeHappinessPlot(happiness, hAxes)
   happinessPlot = plot(hAxes, happiness);
 
   xlabel(hAxes, 'Generation')
-  ylabel(hAxes, 'Average happiness [%]')
+  ylabel(hAxes, 'Average happiness')
   title(hAxes, 'Happiness over time')
 
 end
