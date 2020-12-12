8fc9221c11f8309583d1fc50a88e606bdbad3315
Eken-KN <60141649+Eken-KN@users.noreply.github.com>
2020-12-12 11:52:20 +0100

    Update UpdatePartyFreqPlot.m

--- Election-system/UpdatePartyFreqPlot.m
+++ Election-system/UpdatePartyFreqPlot.m
@@ -1,15 +1,20 @@
 function UpdatePartyFreqPlot(hAxes, government)
 
   nParty = length(hAxes.Children);
-  [~, col] = find(government);
-  
-  % Children are sorted in reverse order
-  for i = 1:size(col, 1)
-    partyIndex = col(i);
+%   [~, col] = find(government);
+%   
+%   % Children are sorted in reverse order
+%   for i = 1:size(col, 1)
+%     partyIndex = col(i);
+%     oldVal = hAxes.Children(nParty-(partyIndex-1)).YData;
+%     hAxes.Children(nParty-(partyIndex-1)).YData = oldVal + 1;
+%   end
+
+  timesSeated = sum(government > 0, 1);
+
+  for partyIndex = 1:size(timesSeated, 2)
     oldVal = hAxes.Children(nParty-(partyIndex-1)).YData;
-    hAxes.Children(nParty-(partyIndex-1)).YData = oldVal + 1;
+    hAxes.Children(nParty-(partyIndex-1)).YData = oldVal + timesSeated(partyIndex);
   end
-
-
 
 end
